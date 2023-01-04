d543359f879870d8dd0be4c8c243b2495a612a45
Andhika Dwitama <andhika.dwitama@ptbsp.com>
2023-01-04 13:22:13 +0700

    feath(auth): handling jwt authorization
    - add jwt configuration
    - add service for handle jwt
    - add utility function for auth
    - add jwt provider with axios

---
+++ src/auth/jwt/jwtConfig.js
@@ -0,0 +1,10 @@
+export default {
+  // endpoints
+  loginEndpoint: '/auth/login',
+  logoutEndpoint: '/auth/logout',
+  refreshEndpoint: '/auth/refresh',
+
+  // localStorage key for tokens
+  storageAccessTokenKey: 'accessToken',
+  storageRefreshTokenKey: 'refreshToken',
+}
