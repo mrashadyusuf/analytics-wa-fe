8ee2df6877051589037ab6dff8094ab242573918
Andhika Dwitama <andhika.dwitama@ptbsp.com>
2023-01-16 17:12:45 +0700

    style(swal2): fix button style
    - remove v-btn--size-default class conflicting padding

--- src/plugins/vue-sweetalert2.js
+++ src/plugins/vue-sweetalert2.js
@@ -2,10 +2,10 @@ import swal from 'vue-sweetalert2'
 
 const swalOptions = {
   customClass: {
-    closeButton: 'v-btn v-btn--size-default',
-    confirmButton: 'v-btn v-btn--size-default',
-    denyButton: 'v-btn v-btn--size-default',
-    cancelButton: 'v-btn v-btn--size-default',
+    closeButton: 'v-btn',
+    confirmButton: 'v-btn',
+    denyButton: 'v-btn',
+    cancelButton: 'v-btn',
   },
 }
 
